ed65dfd485e71920f371ae6683b1571ab90faf33
Aaron Faanes <dafrito@gmail.com>
2021-12-06 23:25:29 -0600

    Rename to log

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 import {assert} from "chai";
-import todo from "../dist/parsegraph-TODO-PACKAGE-NAME";
+import todo from "../dist/parsegraph-log";
 
 describe("Package", function () {
   it("works", ()=>{
